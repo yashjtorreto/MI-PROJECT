9124169dfdbaa3dda2bf3120c0aa9ea728176db4
yashjtorreto <88072805+yashjtorreto@users.noreply.github.com>
2022-04-08 00:36:44 +0530

    Add files via upload

--- ml minip/app.py
+++ ml minip/app.py
@@ -1,3 +1,4 @@
+from msilib.schema import Property
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
@@ -5,16 +6,33 @@ model = pickle.load(open('travelo.pkl', 'rb'))
 
 app = Flask(__name__)
 
-@app.route('/',methods=['POST'])
+@app.route('/',methods=['GET'])
 def helloworld():
     return render_template('MyTrip.html')
 
-@app.route('/MyTrip_predict',methods=['POST'])
-def insurance():
+@app.route('/next_predict',methods=['POST'])
+def travel():
+    CustomerId=request.form['CustomerId']
+    ProdTaken=request.form['ProdTaken']
     Age=request.form['Age']
+    HowwasCustomerContacted=request.form['How was Customer Contacted']
+    CityTire=request.form['City Tire']
+    PitchDuration=request.form['Pitch Duration']
+    occupation=request.form['occupation']
+    gender=request.form['gender']
+    NoofTravellers=request.form['No of Travellers']
+    Product=request.form['Product']
+    PropertyRatings=request.form['Property Ratings']
+    MaritalStatus=request.form['Marital Status']
+    NoofTripsperYear=request.form['No of Trips per Year']
+    Passport=request.form['Passport']
+    PitchRatings=request.form['Pitch Ratings']
+    Car=request.form['Car']
+    NoofChildren=request.form['No of Children']
+    designation=request.form['designation']
     MonthlyIncome=request.form['Monthly Income']
 
-    arr = np.array([[Age,MonthlyIncome]])
+    arr = np.array([[CustomerId,ProdTaken,Age,HowwasCustomerContacted,CityTire,PitchDuration,occupation,gender,NoofTravellers,Product,PropertyRatings,MaritalStatus,Passport,PitchRatings,Car,NoofChildren,designation,MonthlyIncome,NoofTripsperYear]])
 
     pred = model.predict(arr)
 
